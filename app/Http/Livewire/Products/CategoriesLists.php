0503fae0e9092ffdbbff4e38932a5f82ba87f496
Ubuntu <ubuntu@ip-172-31-28-184.us-east-2.compute.internal>
2021-12-08 18:06:56 +0000

    livewire components

---
+++ app/Http/Livewire/Products/CategoriesLists.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Products;
+
+use Livewire\Component;
+
+class CategoriesLists extends Component
+{
+    public function render()
+    {
+        return view('livewire.products.categories-lists');
+    }
+}
