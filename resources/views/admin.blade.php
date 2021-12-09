fa27191be106dcb6c925af47e70a059d2e9a24d7
tijjvni <tijjvni@gmail.com>
2021-12-09 15:30:53 +0000

    message

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -8,7 +8,8 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                
+                <livewire:products.categories-create />
+                <livewire:products.categories-lists />
             </div>
         </div>
     </div>
