0989dc0874b211acf29285dc76fef9b1e592c0fc
tijjvni <tijjvni@gmail.com>
2021-12-09 15:37:35 +0000

    message

--- resources/views/livewire/products/lists.blade.php
+++ resources/views/livewire/products/lists.blade.php
@@ -1,3 +1,27 @@
 <div>
-    {{-- Be like water. --}}
+<table class="table-auto w-full">
+        <thead>
+        <tr>
+            <th class="px-4 py-2">S/N</th>
+            <th class="px-4 py-2">Product</th>
+            <th class="px-4 py-2">Price</th>
+            <th class="px-4 py-2">Msl</th>
+            <th class="px-4 py-2">Qty</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach ($products as $product)
+            <tr @if($loop->even)class="bg-grey"@endif>
+                <td class="border px-4 py-2">{{ $lood->index }}</td>
+                <td class="border px-4 py-2">
+                    <p>{{ $product->name }}</p>
+                    <p>{{ $product->description }}</p>
+                </td>
+                <td class="border px-4 py-2">N{{ number_format($product->price) }}</td>
+                <td class="border px-4 py-2">{{ $product->msl }}</td>
+                <td class="border px-4 py-2">{{ $product->qty }}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
 </div>
