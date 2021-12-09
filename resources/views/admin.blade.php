b4b4aa3dc49dbb01cfaacfd58e37342d7fff0cc4
tijjvni <tijjvni@gmail.com>
2021-12-09 15:47:26 +0000

    mesage

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -6,11 +6,9 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <livewire:products.categories-create />
-                <livewire:products.categories-lists />
-            </div>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-2">
+            <livewire:products.categories-create />
+            <livewire:products.categories-lists />
         </div>
     </div>
 </x-app-layout>
--- resources/views/livewire/products/categories-lists.blade.php
+++ resources/views/livewire/products/categories-lists.blade.php
@@ -7,13 +7,17 @@
         </tr>
         </thead>
         <tbody>
-        @foreach ($categories as $category)
+        @forelse ($categories as $category)
             <tr @if($loop->even)class="bg-grey"@endif>
                 <td class="border px-4 py-2">{{ $catgeory->name }}</td>
                 <td class="border px-4 py-2">{{ $category->products()->count() }}</td>
 
             </tr>
-        @endforeach
+        @empty
+            <tr>
+                <td class="border px-4 py-2 text-center text-gray-400" colspan="2">List is empty</td>
+            </tr>
+        @endforelse
         </tbody>
     </table>
 </div>
