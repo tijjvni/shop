d762c9a329e34df94db4ebe2d3da04f5dc59632a
tijjvni <tijjvni@gmail.com>
2021-12-10 06:47:40 +0000

    message

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -6,4 +6,7 @@
     <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
     </div>
+
+
+    <p class="text-center tetxt-xs">Designed and developed with love by <a href="https://delitechsolutions.com">Delitech Solutions</a></p>
 </div>
