401d4924397a4cc7e484ade0c050da1d845987fb
tijjvni <tijjvni@gmail.com>
2021-12-10 02:58:49 +0000

    mssge

--- resources/views/livewire/products/create.blade.php
+++ resources/views/livewire/products/create.blade.php
@@ -1,2 +1,26 @@
 <div>
+
+    <x-jet-form-section submit="createProduct">
+        <x-slot name="title">
+            {{ __('Create Product') }}
+        </x-slot>
+
+        <x-slot name="description">
+            {{ __('Create a new product.') }}
+        </x-slot>
+
+        <x-slot name="form">     
+ 
+        </x-slot>
+
+        <x-slot name="actions">
+            <x-jet-action-message class="mr-3" on="saved">
+                {{ __('Saved.') }}
+            </x-jet-action-message>
+
+            <x-jet-button>
+                {{ __('Save') }}
+            </x-jet-button>
+        </x-slot>
+    </x-jet-form-section>    
 </div>
