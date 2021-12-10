648ad0f88e3f08bdd31e159e03f228b045679056
tijjvni <tijjvni@gmail.com>
2021-12-10 03:00:10 +0000

    mssge

--- resources/views/livewire/products/create.blade.php
+++ resources/views/livewire/products/create.blade.php
@@ -10,7 +10,11 @@
         </x-slot>
 
         <x-slot name="form">     
- 
+        <div class="w-full space-y-2 col-span-6 space-y-1">
+                <x-jet-label for="name" value="{{ __('Name') }}"/>
+                <x-jet-input id="name" class="block w-full" type="text" placeholder="Product name" wire:model.defer="name"/>
+                <x-jet-input-error for="name" />
+            </div> 	
         </x-slot>
 
         <x-slot name="actions">
